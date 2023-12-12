22a0a0bfd496ab65f669a500992c0ca671a64938
MuhammadRifatA <mrifatanwar36@gmail.com>
2023-12-12 13:09:26 +0700

    upload week11

---
+++ week-11/master_plan/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:master_plan/views/plan_creator_screen.dart';
+import './models/plan.dart';
+import './provider/plan_provider.dart';
+
+void main() => runApp(const MasterPlanApp());
+
+class MasterPlanApp extends StatelessWidget {
+  const MasterPlanApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PlanProvider(
+      notifier: ValueNotifier<List<Plan>>(const []),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'State management app',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const PlanCreatorScreen(),
+      ),
+    );
+  }
+}
